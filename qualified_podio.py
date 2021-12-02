98307482ca1e64bc4291edce7cf22ae581ad68c7
lu-taynara <lu_taynara22@hotmail.com>
2021-12-02 21:23:01 -0200

    atualizacao do codigo

--- qualified_podio.py
+++ qualified_podio.py
@@ -3,7 +3,7 @@ tempo_chegada2 = 90
 tempo_chegada3 = 110
 
 def podio_olimpico(tempo_chegada1, tempo_chegada2, tempo_chegada3):
-    # Implemente aqui a lógica da função
+ 
     tempo = sorted([tempo_chegada1, tempo_chegada2, tempo_chegada3])
     return (f'1 - {tempo[0]} minutos\n2 - {tempo[1]} minutos\n3 - {tempo[2]} minutos\n')
   
